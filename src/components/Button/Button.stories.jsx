3b2fb1a4c28cb2b609949750270e03d1011447a9
AOMuiz <abdulwasiumuiz@gmail.com>
2022-12-25 15:58:09 +0100

    add index,js in component folder and remove unncessary comments in button stories

--- src/components/Button/Button.stories.jsx
+++ src/components/Button/Button.stories.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Button from "./index";
 
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   title: "Components/Button",
   component: Button,
@@ -11,11 +10,10 @@ export default {
   },
 };
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template = (args) => <Button {...args} />;
 
 export const Primary = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
+
 Primary.args = {
   label: "Button",
 };
