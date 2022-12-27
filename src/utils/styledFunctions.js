c1bf7c278235e6dedea4ec1d211c96e2686666fc
AOMuiz <abdulwasiumuiz@gmail.com>
2022-12-28 00:52:07 +0100

    add space styled function to utils

--- src/utils/styledFunctions.js
+++ src/utils/styledFunctions.js
@@ -7,27 +7,39 @@ export const colorsFn = css`
   background-color: ${(props) => props.bg && props.bg};
 `;
 
-export const spaceFn = (size = "standard") => {
-  return {
-    standard: css`
-      padding: 8px 28px;
-      height: 40px;
-      font-size: 1rem;
-    `,
-    small: css`
-      /* width: 75px; */
-      height: 32px;
-      padding: 4px 20px;
-      font-size: ${14 / 16}rem;
-    `,
-    large: css`
-      padding: 16px 40px;
-      font-size: ${18 / 16}rem;
-    `,
-  }[size];
-};
+export const spaceFn = css`
+  margin: ${(props) => props.m && props.m};
+  /** Margin on top */
+  margin-top: ${(props) => props.m && props.m};
+  /** Margin on right */
+  margin-right: ${(props) => props.mr && props.mr};
+  /** Margin on bottom */
+  margin-bottom: ${(props) => props.mb && props.mb};
+  /** Margin on left */
+  margin-left: ${(props) => props.ml && props.ml};
+  /** Margin on left and right */
+  margin-inline: ${(props) => props.mx && props.mx};
+  /** Margin on top and bottom */
+  margin-block: ${(props) => props.my && props.my};
+  /** Padding on top, left, bottom and right */
+  padding: ${(props) => props.p && props.p};
+  /** Padding on top */
+  padding-top: ${(props) => props.pt && props.pt};
+  /** Padding on right */
+  padding-right: ${(props) => props.pr && props.pr};
+  /** Padding on bottom */
+  padding-bottom: ${(props) => props.pb && props.pb};
+  /** Padding on left */
+  padding-left: ${(props) => props.pl && props.pl};
+  /** Padding on left and right */
+  padding-inline: ${(props) => props.px && props.px};
+  /** Padding on top and bottom */
+  padding-block: ${(props) => props.py && props.py};
+`;
 
 export let fontSizesFn = css`
   font-size: ${(props) => props.fontSize && props.fontSize};
   font-weight: ${(props) => props.weight && props.weight};
 `;
+
+// spaceFn.propTypes = {}
