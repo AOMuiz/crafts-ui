c4bdb95128985959451465da7a100ca395f6f5d9
AOMuiz <abdulwasiumuiz@gmail.com>
2022-12-28 17:33:55 +0100

    add gradient and background styled functions

--- src/utils/styledFunctions.js
+++ src/utils/styledFunctions.js
@@ -4,19 +4,38 @@ import { css } from "styled-components";
 
 export const colorsFn = css`
   color: ${(props) => props.color && props.color};
-  background-color: ${(props) => props.bg && props.bg};
+  background: ${(props) => props.bg && props.bg};
+  background-color: ${(props) => props.bgColor && props.bgColor};
+`;
+
+export const gradientFn = css`
+  background-clip: ${(props) => props.bgClip && props.bgClip};
+  background-image: ${(props) => props.bgGradient && props.bgGradient};
+`;
+
+export const backgroundFn = css`
+  background: ${(props) => props.bg && props.bg};
+  background-image: ${(props) => props.bgImage && props.bgImage};
+  background-size: ${(props) => props.bgSize && props.bgSize};
+  background-position: ${(props) => props.bgPosition && props.bgPosition};
+  background-repeat: ${(props) => props.bgRepeat && props.bgRepeat};
+  background-attachment: ${(props) => props.bgAttachment && props.bgAttachment};
 `;
 
 export const spaceFn = css`
   margin: ${(props) => props.m && props.m};
   /** Margin on top */
-  margin-top: ${(props) => props.m && props.m};
+  margin-top: ${(props) => props.mt && props.mt};
   /** Margin on right */
   margin-right: ${(props) => props.mr && props.mr};
+  /** Margin on end(right or left) for RTL support */
+  margin-inline-end: ${(props) => props.me && props.me};
   /** Margin on bottom */
   margin-bottom: ${(props) => props.mb && props.mb};
   /** Margin on left */
   margin-left: ${(props) => props.ml && props.ml};
+  /** Margin on start(right or left) for RTL support */
+  margin-inline-start: ${(props) => props.ms && props.ms};
   /** Margin on left and right */
   margin-inline: ${(props) => props.mx && props.mx};
   /** Margin on top and bottom */
@@ -27,10 +46,14 @@ export const spaceFn = css`
   padding-top: ${(props) => props.pt && props.pt};
   /** Padding on right */
   padding-right: ${(props) => props.pr && props.pr};
+  /** Margin on start(right or left) for RTL support */
+  padding-inline-end: ${(props) => props.pe && props.pe};
   /** Padding on bottom */
   padding-bottom: ${(props) => props.pb && props.pb};
   /** Padding on left */
   padding-left: ${(props) => props.pl && props.pl};
+  /** Margin on start(right or left) for RTL support */
+  padding-inline-start: ${(props) => props.ps && props.ps};
   /** Padding on left and right */
   padding-inline: ${(props) => props.px && props.px};
   /** Padding on top and bottom */
@@ -41,5 +64,3 @@ export let fontSizesFn = css`
   font-size: ${(props) => props.fontSize && props.fontSize};
   font-weight: ${(props) => props.weight && props.weight};
 `;
-
-// spaceFn.propTypes = {}
